890cc26f290bd3e59947181ef1eca4a4951154a3
FroostySnoowman <49008912+FroostySnoowman@users.noreply.github.com>
2022-12-25 20:16:47 -0700

    Add files via upload

---
+++ dropdowns/order/orderdevelopment.py
@@ -0,0 +1,40 @@
+import discord
+import yaml
+from discord.ext import commands
+
+from modals.order import Order
+
+with open('config.yml', 'r') as file:
+    data = yaml.safe_load(file)
+
+guild_id = data["General"]["GUILD_ID"]
+
+class OrderDevelopmentDropdown(discord.ui.Select):
+    def __init__(self):
+
+        options = [
+            discord.SelectOption(label='Bot Developer'),
+            discord.SelectOption(label='Datapack Developer'),
+            discord.SelectOption(label='Plugin Developer'),
+            discord.SelectOption(label='Mod Developer'),
+            discord.SelectOption(label='App Developer'),
+        ]
+
+        super().__init__(placeholder="Select an option!", min_values=1, max_values=1, options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        category = self.values[0]
+        await interaction.response.send_modal(Order(category))
+
+class OrderDevelopmentDropdownView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+        self.add_item(OrderDevelopmentDropdown())
+
+class OrderDevelopmentCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+async def setup(bot):
+    await bot.add_cog(OrderDevelopmentCog(bot), guilds=[discord.Object(id=guild_id)])
